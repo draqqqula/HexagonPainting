0db65b90a689a44f3b4ab05d1b3846700e38358a
bkorshunov <mrdrakula164@gmail.com>
2025-01-24 13:25:43 +0500

    Рефакторинг Core

---
+++ HexagonPainting.Core/Drawing/DrawingHelper.cs
@@ -0,0 +1,13 @@
+﻿using HexagonPainting.Core.Drawing.Interfaces;
+using HexagonPainting.Core.Map.Interfaces;
+
+namespace HexagonPainting.Core.Drawing;
+
+public static class DrawingHelper
+{
+    public static void Draw<TColor>(IHexagonMap<TColor> map, IBrush<TColor> brush)
+    {
+        var figure = brush.Draw();
+        figure.ApplyTo(map);
+    }
+}
--- HexagonPainting.Core/Map/Interfaces/IHexagonMap.cs
+++ HexagonPainting.Core/Map/Interfaces/IHexagonMap.cs
@@ -6,4 +6,6 @@ namespace HexagonPainting.Core.Map.Interfaces;
 public interface IHexagonMap<TColor> : IEnumerable<TileValue<TColor>>
 {
     public TColor this[GridLocation position] { get; set; }
+    public void Serialize(BinaryWriter writer);
+    public void Deserialize(BinaryReader reader);
 }
