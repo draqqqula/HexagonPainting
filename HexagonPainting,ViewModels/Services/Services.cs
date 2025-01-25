1d253ce3e607d97000756318c79a3b632637bf21
bkorshunov <mrdrakula164@gmail.com>
2025-01-25 07:58:15 +0500

    Исправлен неймспейс

--- HexagonPainting,ViewModels/Services/Services.cs
+++ HexagonPainting,ViewModels/Services/Services.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HexagonPainting_ViewModels;
+namespace HexagonPainting_ViewModels.Services;
 
 public static class Services
 {
